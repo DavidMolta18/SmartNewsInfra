059a18a1e7fb001739d1f14f4689e37fdfbe156b
David Molta Hernández <83254590+DavidMolta18@users.noreply.github.com>
2025-09-01 12:20:44 +0000

    Added output URL

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "cloud_run_url" {
   value = module.cloud_run.url
 }
+
+output "cloud_run_url" {
+  description = "URL pública del servicio en Cloud Run"
+  value       = module.cloud_run.url
+}
+
+output "image_deployed" {
+  description = "Imagen de Docker usada en el despliegue"
+  value       = var.image_url
+}
